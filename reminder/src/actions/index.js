8c26c015a33fb9defa3cc89186d76d0785508a3f
Salman Rana <salman.r.rana@gmail.com>
2017-06-06 14:27:19 -0500

    added remove all reminder button

--- reminder/src/actions/index.js
+++ reminder/src/actions/index.js
@@ -1,4 +1,4 @@
-import { ADD_REMINDER, DELETE_REMINDER } from '../constant';
+import { ADD_REMINDER, DELETE_REMINDER, CLEAR_REMINDERS } from '../constant';
 
 export const addReminder = (text, dueDate) => {
   const action = {
@@ -17,4 +17,10 @@ export const deleteReminder = (id) => {
   }
   console.log('deleting in actions', action);
   return action;
+}
+
+export const clearReminders = () => {
+  return{
+    type: CLEAR_REMINDERS
+  }
 }
